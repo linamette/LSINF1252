85001f906ccabbb823f6b6aad89ffa7bae5c703b
GuillaumeSINF <guillaume.moermans@student.uclouvain.be>
2016-04-27 17:58:37 +0200

    modificationMain

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include "fractal.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    /* TODO */
-
+    char test1[]="-d";
+    char test2[]="--maxthreads";
     return 0;
 }
+
