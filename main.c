6324894927648a561813a8df47ceffa83182391f
Gradzielewski Vincent <vincent.gradzielewski@student.uclouvain.be>
2016-04-28 11:28:48 +0200

    main.c argument

--- main.c
+++ main.c
@@ -13,11 +13,12 @@ int main(int argc, char* argv[]) {
     int files = argc-1;
     int maxthreads = 1;
     int thread = 0;
-    char **filesName;
+
+    //printf("%d\n", argc);
 
     int count = 1;
 
-    while (count <= argc) {
+    while (count <= files) {
     	if (strcmp(argv[count], "--maxthreads") == 0) {
     		thread = count;
     		maxthreads = atoi(argv[count+1]);
@@ -25,25 +26,32 @@ int main(int argc, char* argv[]) {
     	if (strcmp(argv[count], "-d") == 0) {
     		optionD = count;
     	}
+    	//printf("%s\n", argv[count]);
     	count++;
     }
+    //printf("%d\n", thread);
 
-    if (thread != 1) {
+    if (thread != 0) {
     	files = files-2;
    	}
-   	if (optionD != 1) {
+   	if (optionD != 0) {
    		files = files-1;
    	}
 
+   	char *filesName[files];
    	int i = 0;
    	int j = 1;
    	while (files > 0) {
    		if (j != thread && j != optionD && j != thread+1) {
    			filesName[i] = argv[j];
+   			printf("%s\n", filesName[i]);
    			i++;
-   			j++;
    			files--;
    		}
    		j++;
    	}
+   	//printf("%d\n", optionD);
+   	//printf("%d\n", maxthreads);	
+   	return 0;
+
 }
