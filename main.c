34fdd1ada1e1f4a4c1bb0f88bc39ff9cf8c64025
Vincent Gradzielewski <vincent.gradzielewski@gmail.com>
2016-05-02 14:04:11 +0200

    mise au propre avant thread

--- main.c
+++ main.c
@@ -5,8 +5,7 @@
 #include "libfractal/fractal.h"
 #include "libfractal/tools.c"
 
-char* add_char(char *word, char add)
-{
+char* add_char(char *word, char add) {
   	size_t l = strlen(word);
   	char *stock = (char *)malloc(l +1+1);
   	strcpy(stock, word);
@@ -81,15 +80,11 @@ void fractal_calcul (struct fractal *fractal, struct fractal *best_fract, int op
 int main(int argc, char* argv[]) {
 
 	/******* Gère les différentes entrées possibles *******/
-
     int optionD = 0;
     int files = argc-1;
     int maxthreads = 1;
     int thread = 0;
 	int count = 1;
-	struct fractal fract;
-	struct fractal best_fract;
-	fractal_set_average(&best_fract, -8000);
 
     while (count < files) {
     	if (strcmp(argv[count], "--maxthreads") == 0) {
@@ -132,91 +127,81 @@ int main(int argc, char* argv[]) {
    		j++;
    	}
 
+
    	/******************* Lecture des fichiers ******************/
 
     int fileRunner = 0;    //Pour le parcours du tableau contenant les noms d e fichiers
-
     FILE *file = NULL;
+    char currentLine[100]="";//utilisé par fgets suppose que les lignes des fichiers ne dépassent pas 100 char
+    char *beginWord;//détemine le début de chaque mots dans la ligne
+  	char *beginLine = NULL;//pointe vers el début de chaque ligne
 
-    while(fileRunner < files2-1)
-    {
+    char *fractName ="";//prépare un string pour le nom du fractale
+    int fractHeigth;//prépare un int pour la hauteur des fichiers
+    int fractWidth;//""                  "" largeur
+    double fractA;//partie réèlle
+    double fractB;//partie imaginaire
+
+    struct fractal fract;
+	struct fractal best_fract;
+	fractal_set_average(&best_fract, -8000);
+	int result;
+
+    while(fileRunner < files2-1) {
 
         /*Initialisation du fichier courant*/
         file = fopen(filesName[fileRunner],"r");
 
-        char currentLine[100]="";//utilisé par fgets suppose que les lignes des fichiers ne dépassent pas 100 char
-
-        if(file == NULL)
-        {
-          printf("une erreur s'est produite lors de l'initalisation de file\n");
+        if(file == NULL) {
+          	printf("une erreur s'est produite lors de l'initalisation de file\n");
         }
-        else
-        {
-          	char *beginWord;//détemine le début de chaque mots dans la ligne
-          	char *beginLine = NULL;//pointe vers el début de chaque ligne
-
-          	char *fractName ="";//prépare un string pour le nom du fractale
-          	int fractHeigth;//prépare un int pour la hauteur des fichiers
-          	int fractWidth;//""                  "" largeur
-          	double fractA;//partie réèlle
-          	double fractB;//partie imaginaire
-      
+        else {
           	//parcours le fichier ligne par ligne et crée une structure fractale
-          	while(fgets(currentLine, 100, file) != NULL)
-          	{     
+          	while(fgets(currentLine, 100, file) != NULL) {     
             	int i = 0;//compte les espaces
             	int j = 0;//curseur de caractères
             	beginWord ="";
             	beginLine = &currentLine[0];
             	//parcours la ligne en 'arretant à chaque espace pour enregister la donneé nécessaire
-            	while(i < 5)
-            	{
-              		if(*(beginLine+j) ==' ')
-              		{
-                		if(i == 0)
-                		{ 
+            	while(i < 5) {
+              		if(*(beginLine+j) ==' ') {
+                		if(i == 0) { 
                   			fractName = beginWord;
                   			beginWord = "";
                   			//on a le nom du fractal
                   			i++;
                 		}
-                		else if (i == 1)
-                		{
+                		else if (i == 1) {
                   			fractWidth = atoi(beginWord);
                   			beginWord="";
                   			//on a la hauteur du fractacle
                   			i++;
                 		}
-                		else if (i == 2)
-                		{
+                		else if (i == 2) {
                   			fractHeigth = atoi(beginWord);
                   			beginWord = "";
                   			//on a la largeur du fractale
                   			i++;
                 		}
-                		else if (i == 3)
-                		{
+                		else if (i == 3) {
                   			fractA = atof(beginWord);
                   			beginWord="";
                   			//on a la valeur de a
                   			i++;
                 		}
-                		else if (i == 4)
-                		{
+                		else if (i == 4) {
                   			fractB = atof(beginWord);
                   			beginWord = "";
                   			// on a la valeur de b
                   			i++; 
                 		}
-               			else
-                		{
+               			else {
                   			//programmus réparo
                   			printf("probleme de cast\n");
                   			return EXIT_FAILURE;
                 		}
               		}
-              		else
-              		{
+              		else {
                 		beginWord = add_char(beginWord,*(beginLine+j));
               		}
               		j++;
@@ -231,6 +216,6 @@ int main(int argc, char* argv[]) {
     	}
   	}
   	if (optionD == 0) {
-  		int result = write_bitmap_sdl(&best_fract, fractal_get_name(&best_fract));
+  		result = write_bitmap_sdl(&best_fract, fractal_get_name(&best_fract));
   	}
 }//fin du main
